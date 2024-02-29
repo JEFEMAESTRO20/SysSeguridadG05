869a99726cdf09cdaeee6aac816991371ef8ed67
Emerson Damián Lúe Gutiérrez <damianlue20@outlook.com>
2024-02-29 15:56:49 -0600

    Agregue una vista

--- SysSeguridadG05.BL/RolBL.cs
+++ SysSeguridadG05.BL/RolBL.cs
@@ -11,28 +11,28 @@ namespace SysSeguridadG05.BL
 {
     public class RolBL
     {
-        public static async Task<int> CrearAsync(Rol pRol)
+        public async Task<int> CrearAsync(Rol pRol)
         {
             return await RolDAL.CrearAsync(pRol);
 
         }
-        public static async Task<int> ModificarAsync(Rol pRol)
+        public async Task<int> ModificarAsync(Rol pRol)
         {
             return await RolDAL.ModificarAsync(pRol);
         }
-        public static async Task<int> DeleteAsync(Rol pRol)
+        public async Task<int> DeleteAsync(Rol pRol)
         {
             return await RolDAL.DeleteAsync(pRol);
         }
-        public static async Task<Rol> ObtenerPorIdAsync(Rol pRol)
+        public async Task<Rol> ObtenerPorIdAsync(Rol pRol)
         {
             return await RolDAL.ObtenerPorIdAsync(pRol);
         }
-        public static async Task<List<Rol>> ObtenerTodosAsync()
+        public async Task<List<Rol>> ObtenerTodosAsync()
         {
             return await RolDAL.ObtenerTodosAsync();
         }
-        public static async Task<List<Rol>> BuscarAsync(Rol pRol)
+        public async Task<List<Rol>> BuscarAsync(Rol pRol)
         {
             return await RolDAL.BuscarAsync(pRol);
         }
--- SysSeguridadG05.UIMVC/Controllers/RolController.cs
+++ SysSeguridadG05.UIMVC/Controllers/RolController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+//*************************
+using SysSeguridadG05.EN;
+using SysSeguridadG05.BL;
 
 namespace SysSeguridadG05.UIMVC.Controllers
 {
     public class RolController : Controller
     {
-        public IActionResult Index()
+        RolBL rolBl = new RolBL();
+        public async Task<IActionResult> Index(Rol pRol = null)
         {
-            return View();
+            if (pRol == null)
+                pRol = new Rol();
+            if (pRol.Top_Aux == 0)
+                pRol.Top_Aux = 10;
+            else 
+                if(pRol.Top_Aux == -1)
+                pRol.Top_Aux = 0;
+            var roles = await new RolBL().BuscarAsync(pRol);
+            ViewBag.Top = pRol.Top_Aux;
+            return View(roles);
         }
     }
 }
